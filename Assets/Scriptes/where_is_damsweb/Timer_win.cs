890f070d86462a37b7bf0ad2a3fe4704a136fd42
ZoelieV <146029346+ZoelieV@users.noreply.github.com>
2023-12-03 18:56:27 +0100

    The end

---
+++ Assets/Scriptes/where_is_damsweb/Timer_win.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerWin : MonoBehaviour
+{
+    public float timer = 60f;
+    private bool sceneChanged = false;
+    public Text timerText;
+
+    void Update()
+    {
+        if (!sceneChanged)
+        {
+            timer -= Time.deltaTime;
+
+            timerText.text = Mathf.Round(timer).ToString() + "s";
+
+            if (timer <= 0)
+            {
+                ChangeScene();
+            }
+        }
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("win_game");
+    }
+
+    public void SceneChanged()
+    {
+        sceneChanged = true;
+    }
+
+    void DisplayTimer(float TimerToDisplay)
+    {
+        float seconds = Mathf.FloorToInt(TimerToDisplay % 60);
+
+        timerText.text = string.Format("{1:00}s", seconds);
+    }
+}
